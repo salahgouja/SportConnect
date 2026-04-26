924762927dc5c208c3a134f96987b082807f532e
aliammari1 <ammari.ali.0001@gmail.com>
2026-04-26 21:18:04 +0100

    feat: update app version to 1.0.9 across configuration files

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,7 +4,7 @@ class AppConstants {
 
   // App Info
   static const String appName = 'SportConnect';
-  static const String appVersion = '1.0.8';
+  static const String appVersion = '1.0.9';
 
   // Firebase Collections
   static const String usersCollection = 'users';
