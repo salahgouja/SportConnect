36d50e724e42061f8f0bbbabf0edbba2904731d3
aliammari1 <ammari.ali.0001@gmail.com>
2026-04-29 17:02:49 +0100

    update to 1.1.3

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,7 +4,7 @@ class AppConstants {
 
   // App Info
   static const String appName = 'SportConnect';
-  static const String appVersion = '1.1.2';
+  static const String appVersion = '1.1.3';
 
   // Firebase Collections
   static const String usersCollection = 'users';
